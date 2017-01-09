e04fab1e1feb880181985e64364de48411ffe964
Nicola Delle Foglie <dellefoglie.nicola@gmail.com>
2017-01-09 14:40:16 +0100

    Added menu for expansion

--- main.c
+++ main.c
@@ -12,27 +12,47 @@ int main(int argc, char *argv[]) {
 	char name[100];
 	char surname[100];
 	int age;
+	int choose;
 	FILE *out;
 	user rec;
 	
-	printf("[i] Insert your name: ");
-	scanf("%s", &name);
-	printf("[i] Insert your surname: ");
-	scanf("%s", &surname);
-	printf("[i] Insert your age: ");
-	scanf("%d", &age);
-	
-	strcpy(rec.name, name);
-	strcpy(rec.surname, surname);
-	rec.age = age;
-	
-	out = fopen("test.txt", "w");
-	if(out == NULL) {
-		printf("[err] Error while opening the file.");
-		exit(1);
+	while(choose != 3) {
+		system("CLS");
+		printf("Menu:\n");
+		printf("1.\tAdd user\n");
+		printf("3.\tExit\n\n");
+		printf("Choose: ");
+		scanf("%d", &choose);
+		
+		switch(choose) {
+			case 1:
+				system("CLS");
+				out = fopen("test.txt", "a");
+				if(out == NULL) {
+					printf("[err] Error while opening the file.");
+					exit(1);
+				}
+				printf("[i] Insert your name: ");
+				scanf("%s", &name);
+				printf("[i] Insert your surname: ");
+				scanf("%s", &surname);
+				printf("[i] Insert your age: ");
+				scanf("%d", &age);
+				
+				strcpy(rec.name, name);
+				strcpy(rec.surname, surname);
+				rec.age = age;
+				
+				fprintf(out, "%s %s %d\n", rec.name, rec.surname, rec.age);
+				fclose(out);
+				
+				printf("[ok] User added correctly.\n");
+				system("PAUSE");
+				break;
+			default:
+				break;
+		}
 	}
-	fprintf(out, "%s %s %d\n", rec.name, rec.surname, rec.age);
-	fclose(out);
 	
 	return 0;
 }
